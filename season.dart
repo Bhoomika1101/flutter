5b485c939c1ffd9ea85fedc11e795d66c6d22bbb
Bhoomika1101 <bhoomidheeravath@gmail.com>
2025-03-13 13:19:52 +0530

    Create season.dart

---
+++ season.dart
@@ -0,0 +1,12 @@
+class switchd{
+	void season(String seaname)
+	var x = switch(seaname){
+	'winter'||'winter'||'WINTER' => 'November to Feb',
+	'sunny'||'Sunny'||'SUNNY' => 'march to may',
+	'rainy'||'Rainy'||'RAINY' => 'June to july',
+	_=> 'you enter the wrong season';
+};
+	print(x);
+}
+}
+	
